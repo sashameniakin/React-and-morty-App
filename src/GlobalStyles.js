53c7e77c0ed9f49fe401b216e386ff0bf06e82dd
Oleksandr Meniakin <oleksandr.meniakin@gmail.com>
2022-11-11 10:32:53 +0100

    add fetching

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -3,7 +3,9 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
 body {
     margin: 0;
+   
     padding: 0;
+    
     font-family: fantasy;
 }
 *{
